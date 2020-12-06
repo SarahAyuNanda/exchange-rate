d06e7fcc0f91164b3b5ec33e7c50edb0f32d9a8e
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2020-12-06 15:09:41 +0700

    Add router

---
+++ src/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                
+            </Switch>
+        </Router>
+    );
+};
+
+export default AppRouter;
