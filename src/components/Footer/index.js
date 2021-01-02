459dd89b2f50d9db65301f0dc93264b7a8f21aeb
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-01-02 21:59:07 +0700

    Edit year

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Layout } from 'antd';
 
 const { Footer } = Layout
 
 const AppFooter = () => {
+    const myName = 'Sarah Ayu Nanda'
+    const [currentYear, setCurrentYear] = useState('')
+
+    useEffect(() => {
+        let year = new Date().getFullYear()
+        setCurrentYear(year)
+    }, [currentYear])
+
     return (
-        <Footer style={{ textAlign: 'center', padding: 15 }}>2020 &copy; Sarah Ayu Nanda</Footer>
+        <Footer style={{ textAlign: 'center', padding: 15 }}>{currentYear} &copy; {myName}</Footer>
     );
 };
 
