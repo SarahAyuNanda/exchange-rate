3985d3c9df13b43756ee81fc1bbb34c9c0787733
Sarah Ayu Nanda <sarah.nanda@ist.id>
2021-01-02 21:23:27 +0700

    Edit component route

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Login from './containers/Login';
 import Home from './containers/Home';
 import ExchangeRateByDate from './containers/Exchange/DateRate';
 import ExchangeRateByLatest from './containers/Exchange/LatestRate';
@@ -8,13 +9,14 @@ import ExchangeRateByBase from './containers/Exchange/BaseRate';
 import ExchangeRateBySymbol from './containers/Exchange/SymbolRate';
 import ExchangeRateByHistory from './containers/Exchange/HistoryRate';
 import ExchangeRateByPeriode from './containers/Exchange/PeriodeRate';
-import ExchangeRateByComparison from './containers/Exchange/ComparisonRate';
+import ExchangeRateByPeriodeSymbol from './containers/Exchange/PeriodeSymbolRate';
 import PathName from './PathName';
 
 const AppRouter = () => {
     return (
         <Router>
             <Switch>
+                <Route exact path={PathName.ROOT} component={Login} />
                 <Route exact path={PathName.HOME.BASEURL} component={Home} />
 
                 <Route path={PathName.HOME.DATE.BASEURL} component={ExchangeRateByDate} />
@@ -25,7 +27,7 @@ const AppRouter = () => {
 
                 <Route exact path={PathName.HOME.HISTORY.BASEURL} component={ExchangeRateByHistory} />
                 <Route path={PathName.HOME.HISTORY.HISTORY_PERIODE} component={ExchangeRateByPeriode} />
-                <Route path={PathName.HOME.HISTORY.HISTORY_PERIODE_SYMBOL} component={ExchangeRateByComparison} />
+                <Route path={PathName.HOME.HISTORY.HISTORY_PERIODE_SYMBOL} component={ExchangeRateByPeriodeSymbol} />
             </Switch>
         </Router>
     );
