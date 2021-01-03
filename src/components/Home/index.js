03cfd4b92ef9b4b66e7aeb890046618db7c0cff8
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2021-01-03 22:31:48 +0700

    Set table data

---
+++ src/components/Home/index.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import TableList from '../Table/SimpleTableList';
+
+const CurrencyList = () => {
+    const [currencyList, setCurrencyList] = useState([])
+
+    const codeList = [
+        "CAD", "HKD", "ISK", "PHP", "DKK", "HUF", "CZK", "GBP", "RON", "SEK", "IDR",
+        "INR", "BRL", "RUB", "HRK", "JPY", "THB", "CHF", "EUR", "MYR", "BGN", "TRY",
+        "CNY", "NOK", "NZD", "ZAR", "USD", "MXN", "SGD", "AUD", "ILS", "KRW", "PLN"
+    ]
+
+    const currency = [
+        { code: "USD", name: "US Dollar" },
+        { code: "EUR", name: "Euro" },
+        { code: "GBP", name: "British Pound" },
+        { code: "INR", name: "Indian Rupee" },
+        { code: "AUD", name: "Australian Dollar" },
+        { code: "CAD", name: "Canadian Dollar" },
+        { code: "SGD", name: "Singapore Dollar" },
+        { code: "CHF", name: "Swiss Franc" },
+        { code: "MYR", name: "Malaysian Ringgit" },
+        { code: "JPY", name: "Japanese Yen" },
+        { code: "CNY", name: "Chinese Yuan Renminbi" },
+        { code: "NZD", name: "New Zealand Dollar" },
+        { code: "THB", name: "Thai Baht" },
+        { code: "HUF", name: "Hungarian Forint" },
+        { code: "HKD", name: "Hong Kong Dollar" },
+        { code: "MXN", name: "Mexican Peso" },
+        { code: "ZAR", name: "South African Rand" },
+        { code: "PHP", name: "Philippine Peso" },
+        { code: "SEK", name: "Swedish Krona" },
+        { code: "IDR", name: "Indonesian Rupiah" },
+        { code: "BRL", name: "Brazilian Real" },
+        { code: "TRY", name: "Turkish Lira" },
+        { code: "KRW", name: "South Korean Won" },
+        { code: "ISK", name: "Icelandic Krona" },
+        { code: "DKK", name: "Danish Krone" },
+        { code: "CZK", name: "Czech Koruna" },
+        { code: "RON", name: "Romanian Leu" },
+        { code: "RUB", name: "Russian Rubie" },
+        { code: "HRK", name: "Croatian Kuna" },
+        { code: "BGN", name: "Bulgarian Lev" },
+        { code: "NOK", name: "Norwegian Krone" },
+        { code: "ILS", name: "Israeli Shekel" },
+        { code: "PLN", name: "Polish Zloty" }
+    ]
+
+    useEffect(() => {
+        let store = []
+        codeList.sort().map((codeItem, index) => {
+            currency.map(item => {
+                if (codeItem === item.code) {
+                    store.push({
+                        key: index + 1,
+                        code: item.code,
+                        name: item.name
+                    })
+                }
+            })
+        })
+        setCurrencyList(store)
+    }, [currencyList])
+
+    return (
+        <TableList data={currencyList} />
+    );
+};
+
+export default CurrencyList;
